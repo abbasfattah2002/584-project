280788dbeaac1dc2afd7004e91b8c7384b807393
Ryan Chua <rwchua@umich.edu>
2025-04-02 18:30:08 -0400

    Add ground truth for Johnathan; Clean up

--- evaluation.py
+++ evaluation.py
@@ -6,3 +6,15 @@ con = duckdb.connect()
 con.execute("PRAGMA enable_profiling")
 con.execute("PRAGMA enable_profile")
 register(con)
+
+con.execute("""
+			CREATE TABLE ssa_names  (
+				Year INTEGER,
+				Name VARCHAR,
+				Gender CHAR,
+				Count INTEGER
+			);
+			COPY ssa_names FROM 'fixed_ssa_data.csv'
+			""")
+
+print(con.execute("SELECT Name FROM ssa_names").fetchall())
