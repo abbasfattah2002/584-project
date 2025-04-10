f94517acfda2df190c2c0511a4fde427b1a51a57
Ryan Chua <rwchua@umich.edu>
2025-04-10 11:04:03 -0400

    Modify postgres johnathan soundex export

--- postgres/soundex.sql
+++ postgres/soundex.sql
@@ -1,3 +1,15 @@
 CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
 
-\copy ( SELECT Year, Name, Gender FROM ssa_names WHERE difference(Name, 'Johnathan') = 4) TO './postgres/johnathan-soundex.csv' WITH CSV DELIMITER ',' HEADER;
+EXPLAIN ANALYZE SELECT Year, Name, Gender FROM ssa_names WHERE difference(Name, 'Johnathan') = 4;
+
+CREATE TEMP TABLE tempjohnathan AS SELECT Year, Name, Gender FROM ssa_names WHERE difference(Name, 'Johnathan') = 4;
+
+SELECT 'Accuracy: ' ||
+	(SELECT COUNT(*) FROM tempjohnathan j
+	INNER JOIN johnathan s
+	ON s.Year = j.Year AND s.Name = j.Name AND s.Gender = j.Gender)
+|| '/' || (SELECT COUNT(*) FROM johnathan);
+
+DROP TABLE tempjohnathan;
+
+\copy ( SELECT Year, Name, Gender FROM ssa_names WHERE difference(Name, 'Johnathan') = 4 ORDER BY Year ASC, Name ASC, Gender ASC) TO './postgres/johnathan-soundex.csv' WITH CSV DELIMITER ',';
